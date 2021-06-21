70e73f76c6694652de3e7b4aba1f58c1b46bcb2b
Ashlee Boyer <aboyer@allcampus.com>
2021-06-21 14:38:51 -0400

    Fetching from currentTime endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { hot } from 'react-hot-loader';
 
-const App = () => (
-  <main>
-    <h1>Hello, world!!!</h1>
-  </main>
-);
+const App = () => {
+  const [data, setData] = useState({ res: null, isLoading: true });
+
+  useEffect(() => {
+    fetch('/api/currentTime')
+      .then((res) => res.json())
+      .then(({ currentTime }) =>
+        setData({
+          res: currentTime,
+          isLoading: false,
+        })
+      )
+      .catch((err) => {
+        console.error(err);
+        setData({ res: `Error: ${err}`, isLoading: false });
+      });
+  }, []);
+
+  return (
+    <main>
+      <h1>Hello, world!!!</h1>
+      {data.isLoading ? <p>Loading...</p> : <p>API result: {data.res}</p>}
+    </main>
+  );
+};
 
 export default hot(module)(App);
