4bd72af8f277b067ae64525badf5d551e8c2566c
Ashlee Boyer <aboyer@allcampus.com>
2021-06-21 14:38:29 -0400

    Adding currentTime endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, send_from_directory
+from flask import Flask, jsonify, send_from_directory
+import time
 
 app = Flask(__name__, static_url_path='/static')
 
-@app.route("/")
+@app.route('/')
 def index():
   return send_from_directory('', 'index.html')
+
+@app.route('/api/currentTime')
+def currentTime():
+  return jsonify({ 'currentTime': round(time.time() * 1000) })
